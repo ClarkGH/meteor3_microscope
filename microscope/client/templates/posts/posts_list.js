690a1709f7cb428839f7d080e054fcc41ebe7e43
ClarkGH <clarkhinchcliff@hotmail.com>
2015-01-21 15:24:01 -0700

    Wired collection into the postsList template

--- microscope/client/templates/posts/posts_list.js
+++ microscope/client/templates/posts/posts_list.js
@@ -1,25 +1,5 @@
-var postsData = [
-  {
-    title: 'Batman',
-    url: 'http://www.batman.com'
-  },
-  {
-    title: 'This was built with Meteor',
-    url: 'http://meteor.com'
-  },
-  {
-    title: 'Learn Meteor Here',
-    url: 'http://themeteorbook.com'
-  },
-  {
-    title: 'Check Out My Github',
-    url: 'http://www.github.com/clarkgh'
-  },
-  {
-    title: 'Check Out My Blog',
-    url: 'http://glubnerdglub.blogspot.com/'
-  }
-];
 Template.postsList.helpers({
-  posts: postsData
+  posts: function() {
+    return Posts.find();
+  }
 });
